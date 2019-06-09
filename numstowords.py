65482117ae15a97c27514ea561ca74dc923e2718
mngugi <mngugi7@gmail.com>
2019-06-09 21:34:45 +0300

    Add files via upload

---
+++ numstowords.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  9 21:27:32 2019
+Author: mwangi
+Purpose :  a simple program to convert words to numbers using num2words
+
+"""
+# pip install num2words
+
+from num2words import num2words
+num2words(456)
+
+
