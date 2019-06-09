bafb042ea8302b646f567ab42480f700dace68c9
mngugi <mngugi7@gmail.com>
2019-06-09 19:19:34 +0300

    Add files via upload

---
+++ NaturalNumbers.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Author : Mwangi
+Purpose : Simple program to input and print natural numbers
+"""
+n = int(input("Enter Real Numbers >= 1 ... n : "))
+for i in range(1, n+1):
+    print (i, end = " " )
+    
+    
