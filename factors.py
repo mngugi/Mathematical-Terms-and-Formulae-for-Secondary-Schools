8a9adfe99de8902c2013c9de80c058384aa439cd
mngugi <mngugi7@gmail.com>
2019-06-10 21:40:16 +0300

    Add files via upload

---
+++ factors.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Author : Mwangi
+Purpose : a simple factor function
+"""
+def factor(a,b):
+    if b % a == 0:
+        return True
+    else:
+        return False
+factor()
+
+
