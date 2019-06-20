201f6042617d3963716e469c6ffdd1b35fe389e4
mngugi <mngugi7@gmail.com>
2019-06-20 17:48:42 +0300

    Add files via upload

---
+++ rangeofoddnumbers.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Author : Mwangi
+Purpose : a simple program to print a range of odd numbers
+using a for loop
+"""
+
+# prints odd numbers from 3 to 15
+
+for i in range(3,15,2):
+    print(i)   
+    
+    
---
+++ simpleRange1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Author : Mwangi
+Purpose : a simple program to print a range of 9 numbers
+using a for loop
+"""
+
+for i in range(0, 9):
+    print (i) #prints 012345678
+    
+    
