9ce2f2be52b2432508cd29b57b1a6a526f1c3b7f
mngugi <mngugi7@gmail.com>
2019-06-21 19:42:24 +0300

    Add files via upload

---
+++ divisibility.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Author:Mwangi
+Purpose: A program for variable input
+    
+"""
+# z can be any number greater than 0
+
+z = int(input("Enter number: "))
+result = z % 11 == 0
+print ('the number is divisible by 11', result) 
