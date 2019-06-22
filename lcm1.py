5a25bd58e3ca110aba8410e68c7f23fb3b612558
mngugi <mngugi7@gmail.com>
2019-06-22 20:24:14 +0300

    Add files via upload

---
+++ lcm1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 22 13:14:45 2019
+Author:Mwangi
+Purpose: A program to get lcm using the math function
+"""
+import math 
+a = 15
+b = 20
+
+c = a * b
+
+
+num = math.gcd(15,20)
+
+lcm = c / num 
+
+print ('The leaat Common Multiple for 210, 630 is:', lcm)
+
+
+ 
